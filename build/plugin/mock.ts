808644ff76cc73b189b570fab157d73b4a22a8a6
maggot_code <wowhuanhuan233@163.com>
2021-12-13 16:51:47 +0800

    #

--- build/plugin/mock.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: maggot-code
- * @Date: 2021-11-11 09:54:04
- * @LastEditors: maggot-code
- * @LastEditTime: 2021-11-11 09:54:04
- * @Description: file content
- */
--- src/composables/use-data-table/types/mode.d.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: maggot-code
- * @Date: 2021-11-25 10:50:54
- * @LastEditors: maggot-code
- * @LastEditTime: 2021-11-25 10:50:55
- * @Description: file content
- */
