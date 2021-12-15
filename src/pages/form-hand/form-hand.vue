6ac741acb4edbea5d2991700e5e533f0831a4378
maggot_code <wowhuanhuan233@163.com>
2021-12-15 20:30:01 +0800

    #

--- src/pages/form-hand/form-hand.vue
+++ src/pages/form-hand/form-hand.vue
@@ -2,10 +2,13 @@
  * @Author: maggot-code
  * @Date: 2021-11-22 14:48:59
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-12-15 17:08:31
+ * @LastEditTime: 2021-12-15 20:28:40
  * @Description: file content
 -->
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+// import TestJSON5 from '@/composables/test5.json';
+// console.log(TestJSON5);
+</script>
 
 <template>
     <div class="form-hand">123</div>
