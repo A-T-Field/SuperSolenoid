d4af1002f668f11fae9cd4beed71ee89d400ef48
maggot_code <wowhuanhuan233@163.com>
2021-12-17 17:44:15 +0800

    #

--- src/composables/use-data-form/components/VoidField.tsx
+++ src/composables/use-data-form/components/VoidField.tsx
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-12-17 17:01:39
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-12-17 17:34:25
+ * @LastEditTime: 2021-12-17 17:40:34
  * @Description: file content
  */
 import type { VoidFieldSetupProps } from '../types/Props';
@@ -16,13 +16,13 @@ const setupGrid = (props: VoidFieldSetupProps) => {
     const { vesselProps } = props.model;
 
     const gridItem = useRender(props.children).map(item => {
-        return item;
+        return h(NGridItem, {}, {
+            default: () => item
+        });
     });
 
-    console.log(gridItem.length);
-
     return h(
-        <NGrid {...vesselProps}>
+        <NGrid xGap={12} cols="1 600:2 900:3">
             {{
                 default: () => gridItem
             }}
@@ -32,6 +32,7 @@ const setupGrid = (props: VoidFieldSetupProps) => {
 
 const VoidField = (props: VoidFieldSetupProps) => {
     const { vessel } = props.model;
+
     if (vessel === "Grid") return setupGrid(props);
 
     return h(
