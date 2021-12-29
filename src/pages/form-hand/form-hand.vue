5651e6b5d2a04b92887fb06d534b52467d34047b
maggot_code <wowhuanhuan233@163.com>
2021-12-29 10:44:01 +0800

    #

--- src/pages/form-hand/form-hand.vue
+++ src/pages/form-hand/form-hand.vue
@@ -2,26 +2,15 @@
  * @Author: maggot-code
  * @Date: 2021-11-22 14:48:59
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-12-17 16:58:25
+ * @LastEditTime: 2021-12-29 10:35:59
  * @Description: file content
 -->
 <script setup lang='ts'></script>
 
 <template>
     <div class="form-hand">
-        <n-grid cols="1 600:2 900:3" x-gap="12">
-            <n-form-item-gi>
-                <n-input></n-input>
-            </n-form-item-gi>
-
-            <n-form-item-gi>
-                <n-input></n-input>
-            </n-form-item-gi>
-
-            <n-form-item-gi>
-                <n-input></n-input>
-            </n-form-item-gi>
-        </n-grid>
+        <div></div>
+        <h1>form hand</h1>
     </div>
 </template>
 
